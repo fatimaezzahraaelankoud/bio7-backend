c9f6c9ef489b66d367d67cbd86c01d2f2141dc7d
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2025-12-03 22:35:08 +0100

    Entité Product + Repository

---
+++ bio7-backend/src/main/java/com/bio7/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.bio7.repository;
+
+import com.bio7.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
