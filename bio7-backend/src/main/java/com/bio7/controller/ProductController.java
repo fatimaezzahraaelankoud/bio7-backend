963ce638643a37a4543094af8c51bccefa9bea50
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2025-12-04 20:22:46 +0100

    Service Product + Controller REST + endpoints GET

---
+++ bio7-backend/src/main/java/com/bio7/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.bio7.controller;
+
+import com.bio7.model.Product;
+import com.bio7.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/products")
+public class ProductController {
+  private final ProductService productService ;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
---
+++ bio7-backend/src/main/java/com/bio7/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.bio7.service;
+import com.bio7.model.Product;
+import com.bio7.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository ;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
