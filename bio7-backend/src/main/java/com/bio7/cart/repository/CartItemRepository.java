da0597bc7c1a0559c8f940c3dc843da1be869032
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2026-09-14 21:21:47 +0200

    fix repository method

--- bio7-backend/src/main/java/com/bio7/cart/repository/CartItemRepository.java
+++ bio7-backend/src/main/java/com/bio7/cart/repository/CartItemRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
 
-    Optional<CartItem> findByCartIdAndProductId(String CartId);
+    Optional<CartItem> findByCartIdAndProductId(Long CartId , Long ProductId);
 
 }
